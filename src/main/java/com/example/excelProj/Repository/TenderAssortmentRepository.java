eee384b2543cff479c70b41619687f9212b38c48
mamer-amer <shahzad.iqbal.developer@gmail.com>
2020-09-18 23:12:37 +0500

    psuhed

--- src/main/java/com/example/excelProj/Repository/TenderAssortmentRepository.java
+++ src/main/java/com/example/excelProj/Repository/TenderAssortmentRepository.java
@@ -20,8 +20,8 @@ public interface TenderAssortmentRepository extends JpaRepository<TenderAssortme
     @Query(value = "select * from jobportal.tender_assortments where employer_id=:id && notification_for='employer' ORDER BY tender_assortments.notificationDate DESC",nativeQuery = true)
     List<TenderAssortments> getAcceptOrDeclineNotification(@Param("id") Long id);
 
-    @Modifying
     @Transactional
-    @Query(value = "update tender_assortments set is_applied=:isApplied AND notification_from='recruiter' AND notificaton_for='employer' where (tender_id=:tenderId AND recruiter_id=:recruiterUserId AND employer_id=:employerUserId)")
+    @Modifying
+    @Query(value = "update tender_assortments set is_applied=:isApplied,notification_from='recruiter',notificaton_for='employer' where (tender_id=:tenderId AND recruiter_id=:recruiterUserId AND employer_id=:employerUserId)",nativeQuery = true)
     void updateTender(@Param("isApplied") Boolean isApplied,@Param("tenderId") Long tenderId,@Param("recruiterUserId") Long recruiterUserId,@Param("employerUserId") Long employerUserId);
 }
