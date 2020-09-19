6ec1a8ed9b2019500b29f18b70aff4828344c61f
mamer-amer <shahzad.iqbal.developer@gmail.com>
2020-09-20 02:20:34 +0500

    done

--- src/main/java/com/example/excelProj/Repository/TenderAssortmentRepository.java
+++ src/main/java/com/example/excelProj/Repository/TenderAssortmentRepository.java
@@ -14,10 +14,10 @@ import java.util.List;
 
 public interface TenderAssortmentRepository extends JpaRepository<TenderAssortments,Long> {
 
-    @Query(value = "select * from jobportal.tender_assortments where recruiter_id=:id && notification_for='recruiter' ORDER BY notification_date DESC",nativeQuery = true)
+    @Query(value = "select * from tender_assortments where recruiter_id=:id && notification_for='recruiter' ORDER BY notification_date DESC",nativeQuery = true)
     List<TenderAssortments> getTenderNotifications(@Param("id") Long id);
 
-    @Query(value = "select * from jobportal.tender_assortments where employer_id=:id && notification_for='employer' ORDER BY notification_date DESC",nativeQuery = true)
+    @Query(value = "select * from tender_assortments where employer_id=:id && notification_for='employer' ORDER BY notification_date DESC",nativeQuery = true)
     List<TenderAssortments> getAcceptOrDeclineNotification(@Param("id") Long id);
 
     @Transactional
